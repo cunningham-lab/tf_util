3fe459c874ef0b224b36b70f808515b3890a788c
Sean Bittner <sbittner@dyn-160-39-254-47.dyn.columbia.edu>
2018-09-08 17:08:05 -0400

    psd matrix completion via clique updates

--- stat_util.py
+++ stat_util.py
@@ -1,5 +1,9 @@
 import numpy as np
 from scipy.stats import invwishart, dirichlet, multivariate_normal
+from cvxopt import spmatrix, amd
+import chompack as cp
+from Bron_Kerbosch.bronker_bosch3 import bronker_bosch3
+from Bron_Kerbosch.reporter import Reporter
 
 def get_sampler_func(dist, D):
     if (dist['family'] == 'uniform'):
@@ -231,6 +235,13 @@ def truncated_multivariate_normal_rvs(mu, Sigma):
         count += 1;
     return z;
 
+def cov_error(V,I,J,Sigma):
+    num_elems = len(V);
+    error = 0.0;
+    for i in range(num_elems):
+        error += np.square(V[i] - Sigma[I[i], J[i]]);
+    return error;
+
 def get_GP_Sigma(tau, T, Ts):
     K = np.zeros((T, T));
     for i in range(T):
@@ -240,4 +251,103 @@ def get_GP_Sigma(tau, T, Ts):
             if (i != j):
                 K[j,i] = K[i,j];
     return K;
+
+def get_S_D_graph(x, D, T):
+    I = [];
+    J = [];
+    for t in range(T):
+        for d in range(D):
+            new_inds_Dblock = list(range(t*D+d,(t+1)*D))
+            new_inds_temporal = list(range((t+1)*D+d, D*T, D));
+            num_inds = len(new_inds_Dblock) + len(new_inds_temporal);
+            I = I + new_inds_Dblock + new_inds_temporal;
+            J = J + num_inds*[t*D+d];
+    total_inds = len(I);
+    V = [];
+    for i in range(total_inds):
+        V.append(x[I[i], J[i]]);
+        
+    num_nodes = D*T;
+    NODES = list(range(num_nodes));
+    NEIGHBORS = [];
+    for i in range(num_nodes):
+        NEIGHBORS.append([]);
+    it = 0;
+    for i in NODES:
+        while (it < total_inds):
+            if (J[it] == i):
+                if (I[it] == i):
+                    pass;
+                else:
+                    NEIGHBORS[i].append(I[it]);
+                    NEIGHBORS[I[it]].append(i);
+                it += 1;
+            else:
+                break;
+    return V,I,J,NODES,NEIGHBORS;
+
+def get_S_D_covariance(Sigma_D, taus, T, Ts):
+    D = Sigma_D.shape[0];
+    autocovariances = np.zeros((D, T));
+    Sigma = np.zeros((D*T,D*T));
+    for i in range(D):
+        Sigma_tau_i = get_GP_Sigma(taus[i], T, Ts);
+        autocovariances[i,:] = Sigma_D[i,i]*Sigma_tau_i[0,:];
     
+    for t1 in range(T):
+        t1_ind1 = int(t1*D);
+        t1_ind2 = int((t1+1)*D);
+        Sigma[t1_ind1:t1_ind2,t1_ind1:t1_ind2,] = Sigma_D;
+        for t2 in range(t1+1,T):
+            tau_mat = np.diag(autocovariances[:,t2-t1]);
+            t2_ind1 = int(t2*D);
+            t2_ind2 = int((t2+1)*D);
+            Sigma[t1_ind1:t1_ind2, t2_ind1:t2_ind2] = tau_mat;
+            Sigma[t2_ind1:t2_ind2, t1_ind1:t1_ind2,] = tau_mat;
+        
+    return Sigma;
+
+def get_S_D_ME_covariance(Sigma_D, taus, T, Ts):
+    Sigma = get_S_D_covariance(Sigma_D, taus, T, Ts);
+    D = Sigma_D.shape[0];
+    V,I,J,NODES,NEIGHBORS = get_S_D_graph(Sigma, D, T);
+    report = Reporter('## bron_kerbosch')
+    bronker_bosch3([], set(NODES), set(), report, NEIGHBORS)
+    cliques = report.cliques;
+
+    num_cliques = len(cliques);
+    for i in range(num_cliques):
+        cliques[i] = np.array(cliques[i]);
+
+    R = Sigma;
+    R_invs = [];
+    for t in range(num_cliques):
+        clique_t = cliques[t];
+        R_t = R[np.expand_dims(clique_t, 1),clique_t];
+        R_invs.append(np.linalg.inv(R_t));
+
+    # algorithm 1 (Speed and Kiiveri)
+    Sigma = np.eye(D*T);
+    max_iters = 10000;
+    errors = np.zeros((max_iters,));
+    for it in range(max_iters):
+        for t in range(num_cliques):
+            clique_t = cliques[t];
+            Sigma_t = Sigma[np.expand_dims(clique_t, 1),clique_t];
+            
+            Sigma_inv = np.linalg.inv(Sigma);
+            Sigma_t_inv = np.linalg.inv(Sigma_t);
+            update_t = R_invs[t] - Sigma_t_inv;
+            
+            Sigma_inv[np.expand_dims(clique_t, 1), clique_t] += update_t;
+            Sigma = np.linalg.inv(Sigma_inv);
+        error_it = cov_error(V,I,J,Sigma);
+        if (error_it < 1e-10):
+            break;
+        errors[it] = error_it;
+
+    converged = it < max_iters;
+    return Sigma, converged;
+    
+
+
