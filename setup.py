d6877b27ca6596e1ff59548d3473b1e83b3413da
Sean Bittner <sbittner@dyn-160-39-140-33.dyn.columbia.edu>
2018-10-22 17:06:51 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='tf_util',
+      version='1.0',
+      description='Useful tensorflow libraries.',
+      author='Sean Bittner',
+      author_email='srb2201@columbia.edu',
+      packages=['tf_util', 'tf_util.Bron_Kerbosch'],
+     )
